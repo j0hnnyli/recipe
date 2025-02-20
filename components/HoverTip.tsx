9cf9267f93a9c9b0d4fbce70333c88cedef7a974
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-02-19 23:12:46 -0500

    Update: Fix Hydrating Problems with Hover Tip Component (#19)
    * added a quick add feature UI
    
    * fixed hydrating problem with the hover tip component

--- components/HoverTip.tsx
+++ components/HoverTip.tsx
@@ -16,7 +16,7 @@ const HoverTip = ({ children, tip, time } : Props) => {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={time}>
-        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipContent className="text-sm bg-black text-primary_yellow">
           <p>{tip}</p>
         </TooltipContent>

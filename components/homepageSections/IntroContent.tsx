dea102fc6a41eb335c4b640d68f4af51a4fbfccc
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-04-20 02:33:33 -0400

    Bug : Fixed The Flash Before The Animation Starts (#16)
    * redesigned a new landing page
    
    * added scrool top button feature in the footer
    
    * fixed the flash on intro section

--- components/homepageSections/IntroContent.tsx
+++ components/homepageSections/IntroContent.tsx
@@ -11,12 +11,17 @@ const IntroContent = () => {
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.from('.fade-in-item', {
+      gsap.fromTo('.fade-in-item', {
         x : 20,
         opacity : 0,
+      },
+      {
+        x : 0,
+        opacity : 1,
         stagger : 0.2,
         duration : 0.5,
-      })
+      }
+    )
       
       return () => ctx.revert();
     }, fadeInCaontainer)
@@ -28,35 +33,35 @@ const IntroContent = () => {
         ref={fadeInCaontainer}
         className='flex flex-col gap-10 md:w-[80%] mx-auto'
       >
-        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item">
+        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item opacity-0">
           <div className="text-4xl 2xl:text-6xl mb-2">
             <FaUtensils className="text-primary_yellow"></FaUtensils>
           </div>
           <p className="text-xl 2xl:text-3xl">Variety of Recipes</p>
         </div>
 
-        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item">
+        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item opacity-0">
           <div className="text-4xl 2xl:text-6xl mb-2">
             <FaClock className="text-primary_yellow"></FaClock>
           </div>
           <p className="text-xl 2xl:text-3xl">Quick & Easy Recipes</p>
         </div> 
 
-        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item">
+        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item opacity-0">
           <div className="text-4xl 2xl:text-6xl mb-2">
             <TiWorld className="text-primary_yellow"></TiWorld>
           </div>
           <p className="text-xl 2xl:text-3xl">Variety of Cultures</p>
         </div>
 
-        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item">
+        <div className="flex items-center text-white w-full justify-between border-b-2 fade-in-item opacity-0">
           <div className="text-4xl 2xl:text-6xl mb-2">
             <FaHeart className="text-primary_yellow"></FaHeart>
           </div>
           <p className="text-xl 2xl:text-3xl">Personalized Recipe List</p>
         </div>            
       
-        <div className="flex items-center text-primary_yellow justify-center fade-in-item">
+        <div className="flex items-center text-primary_yellow justify-center fade-in-item opacity-0">
           <FaFacebook className="text-4xl mr-5 hover:text-white cursor-pointer" />
           <FaSquareXTwitter className="text-4xl mr-5 hover:text-white cursor-pointer" />
           <FaInstagramSquare className="text-4xl mr-5 hover:text-white cursor-pointer" />

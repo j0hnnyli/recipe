a5cefca276443d8a51b2a0dc8cbe9399c1c8a59a
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-04-20 17:57:21 -0400

    added pointer for links in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,25 +20,26 @@ const Footer = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-3 gap-10">
+        <div className="flex gap-10">
           <div className="flex flex-col gap-2">
             <h3 className="text-lg font-semibold text-primary_yellow">Socials</h3>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Facebook</div>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">X/Twitter</div>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Instagram</div>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3 className="text-lg font-semibold text-primary_yellow">Legal</h3>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Privacy Policy</div>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Terms & Conditions</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Facebook</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">X/Twitter</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Instagram</div>
           </div>
 
           <div className="flex flex-col gap-2">
             <h3 className="text-lg font-semibold text-primary_yellow">Contact</h3>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Email Us</div>
-            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors">Support</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Email Us</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Support</div>
           </div>
+          
+          <div className="flex flex-col gap-2">
+            <h3 className="text-lg font-semibold text-primary_yellow">Legal</h3>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Privacy Policy</div>
+            <div className="text-sm text-gray-400 hover:text-yellow-400 transition-colors cursor-pointer">Terms & Conditions</div>
+          </div>
+
         </div>
       </div>
 

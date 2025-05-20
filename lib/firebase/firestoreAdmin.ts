607f7ea2468e7275dbbaca2fb5f32717314273cd
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-05-20 00:30:21 -0400

    cleaned up code for final touches

--- lib/firebase/firestoreAdmin.ts
+++ lib/firebase/firestoreAdmin.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
 
 if (!admin.apps.length) {
   admin.initializeApp({
